6b80a42ad22e8260d191ab11bcf8e5656b6c3f0d
naman jain <naman.nihi@gmail.com>
2019-09-22 17:49:18 +0530

    all layout added

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.ImageButton
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -28,6 +31,20 @@ class MainActivity : AppCompatActivity() {
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+    fun cal(view: View){
+        val cd= view as ImageButton
+        when(cd.id){
+            bu1.id->{ var intent= Intent(applicationContext,Main2Activity::class.java)
+                startActivity(intent)}
+            bu2.id->{ var intent=Intent(applicationContext,Main3Activity::class.java)
+                startActivity(intent)}
+            bu3.id->{ var intent=Intent(applicationContext,Main4Activity::class.java)
+                startActivity(intent)}
+            bu4.id->{ var intent=Intent(applicationContext,Main5Activity::class.java)
+                startActivity(intent)}
+
         }
     }
 
